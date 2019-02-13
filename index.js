4db349620525fe3663a7c0fefe62f85fa169838e
Michał Łaszczewski <m8@em8.pl>
2019-02-13 12:27:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const RethinkObservableValue = require('./lib/RethinkObservableValue.js')
+const RethinkObservableList = require('./lib/RethinkObservableList.js')
+
+module.exports = { RethinkObservableList, RethinkObservableValue }
